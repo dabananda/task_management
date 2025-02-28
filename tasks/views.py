5c4bfec28a850d6009c4ffbb5f083fd3e0e4d4a2
dabananda <imdmitra@gmail.com>
2025-02-28 16:44:52 +0600

    Implemented create task method

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from tasks.forms import TaskForm
-from tasks.models import Employee
+from tasks.models import Employee, Task
 
 
 def show_task(request):
@@ -19,5 +19,24 @@ def user_dashboard(request):
 def create_task(request):
     employees = Employee.objects.all()
     form = TaskForm(employees=employees)
+
+    if request.method == "POST":
+        form = TaskForm(request.POST, employees=employees)
+        if form.is_valid():
+            data = form.cleaned_data
+            title = data.get("title")
+            description = data.get("description")
+            due_date = data.get("due_date")
+            assigned_to = data.get("assigned_to")
+
+            task = Task.objects.create(
+                title=title, description=description, due_date=due_date)
+
+            for emp_id in assigned_to:
+                employee = Employee.objects.get(id=emp_id)
+                task.assigned_to.add(employee)
+
+            return HttpResponse("Task added successfully!")
+
     context = {"form": form}
     return render(request, "task-form.html", context)
