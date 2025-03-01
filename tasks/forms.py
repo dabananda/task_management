0ea49f51b1d4e44074049cba0c9d8614cf4c1303
dabananda <imdmitra@gmail.com>
2025-03-01 16:14:55 +0600

    Added mixing to forms

--- tasks/forms.py
+++ tasks/forms.py
@@ -2,23 +2,41 @@ from django import forms
 from tasks.models import Task
 
 
-class TaskModelForm(forms.ModelForm):
+class StyledFormMixing:
+    default_classes = "w-full border-2 border-solid border-slate-800 px-2 py-1"
+
+    def apply_styled_widgets(self):
+        for field_name, field in self.fields.items():
+            if isinstance(field.widget, forms.TextInput):
+                field.widget.attrs.update({
+                    'class': self.default_classes,
+                    'placeholder': f"Enter {field.label.lower()}"
+                })
+            elif isinstance(field.widget, forms.Textarea):
+                field.widget.attrs.update({
+                    'class': self.default_classes,
+                    'rows': 5,
+                    'placeholder': f"Enter {field.label.lower()}"
+                })
+            elif isinstance(field.widget, forms.SelectDateWidget):
+                field.widget.attrs.update({
+                    'class': "border-2 border-solid border-slate-800 px-2 py-1"
+                })
+            elif isinstance(field.widget, forms.CheckboxSelectMultiple):
+                field.widget.attrs.update({
+                    'class': "px-2 py-1"
+                })
+
+
+class TaskModelForm(StyledFormMixing, forms.ModelForm):
     class Meta:
         model = Task
         fields = ['title', 'description', 'due_date', 'assigned_to']
         widgets = {
-            'title': forms.TextInput(attrs={
-                'class': 'w-full border-2 border-solid border-slate-800 px-2 py-1',
-                'placeholder': 'Enter task title'
-            }),
-            'description': forms.Textarea(attrs={
-                'class': 'w-full border-2 border-solid border-slate-800 px-2 py-1',
-                'placeholder': 'Enter task description'
-            }),
-            'due_date': forms.SelectDateWidget(attrs={
-                'class': 'border-2 border-solid border-slate-800 px-2 py-1',
-            }),
-            'assigned_to': forms.CheckboxSelectMultiple(attrs={
-                'class': 'px-2 py-1',
-            }),
+            'due_date': forms.SelectDateWidget,
+            'assigned_to': forms.CheckboxSelectMultiple
         }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.apply_styled_widgets()
