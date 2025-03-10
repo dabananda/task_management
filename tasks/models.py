88291212869a34adb0071fa929220607b3cb816a
dabananda <imdmitra@gmail.com>
2025-03-10 09:54:12 +0600

    Django Signal - Email Sending & env variable added

--- tasks/models.py
+++ tasks/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.db.models.signals import post_save, pre_save, m2m_changed
+from django.dispatch import receiver
+from django.core.mail import send_mail
 
 
 class Employee(models.Model):
@@ -56,3 +59,18 @@ class TaskDetail(models.Model):
 
     def __str__(self):
         return f"Task details from {self.task.title}"
+
+
+@receiver(m2m_changed, sender=Task.assigned_to.through)
+def notify_employess_on_task_creation(sender, instance, action, **kwargs):
+    if action == "post_add":
+        assigned_emails = [
+            employee.email for employee in instance.assigned_to.all()
+        ]
+
+        send_mail(
+            "New Task Assigned",
+            f"You have been assigned to task: {instance.title}",
+            "satyajitroy2k20@gmail.com",
+            assigned_emails,
+        )
